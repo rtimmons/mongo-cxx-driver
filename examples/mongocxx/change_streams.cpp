3e708edc6dd238c442303b0c86a1f7aac645dbc2
Ryan Timmons <ryan.timmons@10gen.com>
2018-04-16 13:03:28 -0400

    more complete example

--- examples/mongocxx/change_streams.cpp
+++ examples/mongocxx/change_streams.cpp
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <cstdlib>
 #include <iostream>
 
 #include <bsoncxx/builder/basic/document.hpp>
@@ -22,70 +21,62 @@
 #include <mongocxx/change_stream.hpp>
 #include <mongocxx/client.hpp>
 #include <mongocxx/instance.hpp>
-#include <mongocxx/stdx.hpp>
+#include <mongocxx/pool.hpp>
 #include <mongocxx/uri.hpp>
 
-#include <get_server_version.h>
+std::string get_server_version(const mongocxx::client& client) {
+    bsoncxx::builder::basic::document server_status{};
+    server_status.append(bsoncxx::builder::basic::kvp("serverStatus", 1));
+    bsoncxx::document::value output = client["test"].run_command(server_status.extract());
 
-using bsoncxx::builder::basic::kvp;
-using bsoncxx::builder::basic::make_document;
+    return bsoncxx::string::to_string(output.view()["version"].get_utf8().value);
+}
 
-int main(int, char**) {
-    // The mongocxx::instance constructor and destructor initialize and shut down the driver,
-    // respectively. Therefore, a mongocxx::instance must be created before using the driver and
-    // must remain alive for as long as the driver is in use.
+void watch_forever(mongocxx::pool& pool) {
+    auto client = pool.acquire();
+
+    // Wait up to 1 second before polling again.
+    const std::chrono::milliseconds await_time{1000};
+
+    mongocxx::options::change_stream options;
+    options.max_await_time(await_time);
+
+    auto collection = (*client)["db"]["coll"];
+    mongocxx::change_stream stream = collection.watch(options);
+
+    while (true) {
+        for (const auto& event : stream) {
+            std::cout << bsoncxx::to_json(event) << std::endl;
+        }
+    }
+}
+
+int main() {
     mongocxx::instance inst{};
-    mongocxx::client conn{mongocxx::uri{}};
+    mongocxx::uri uri{"mongodb://localhost:27017/?minPoolSize=3&maxPoolSize=3"};
+    mongocxx::pool pool{uri};
 
-    // Change streams require 3.6
-    if (get_server_version(conn) < "3.6") {
-        return 0;
-    }
-
-    auto coll = conn["test"]["coll"];
-    coll.drop();
-
-    // Dummy insert to create the database and collection.
-    coll.insert_one(make_document(kvp("dummy", "document")));
-
-    {
-        // Iterate over empty change-stream (no events):
-        mongocxx::change_stream stream = coll.watch();
-        for (const auto& event : stream) {
-            std::cout << bsoncxx::to_json(event) << std::endl;
-        }
-    }
-
-    {
-        // Iterate over a stream with events:
-        mongocxx::change_stream stream = coll.watch();
-        coll.insert_one(make_document(kvp("some", "event")));
-        for (const auto& event : stream) {
-            std::cout << bsoncxx::to_json(event) << std::endl;
-        }
-    }
-
-    {
-        // A "forever" loop that continuously processes events until exceptions are encountered.
-        // Batch 100 events at a time and wait up to 1 second before polling again.
-
-        // Configure appropriate values for your application:
-        const std::int32_t batch_size{100};
-        const std::chrono::milliseconds await_time{1000};
-        const mongocxx::stdx::optional<long> max_iterations =
-            1;  // Use an empty optional for no max
-
-        mongocxx::options::change_stream options;
-        options.batch_size(batch_size);
-        options.max_await_time(await_time);
-
-        mongocxx::change_stream stream = coll.watch(options);
-        long iteration = 0;
-        while (!max_iterations || iteration++ < max_iterations) {
-            // Server errors propagate as exceptions and will cause our loop to exit.
-            for (const auto& event : stream) {
-                std::cout << bsoncxx::to_json(event) << std::endl;
+    try {
+        {
+            auto client = pool.acquire();
+            if (get_server_version(*client) < "3.6") {
+                return 1;
             }
         }
+
+        watch_forever(pool);
+
+        return 0;
+    } catch (...) {
+        auto e = std::current_exception();
+        try {
+            if (e) {
+                std::rethrow_exception(e);
+            }
+        } catch (const std::exception& e) {
+            std::cerr << "Caught exception \"" << e.what() << "\"" << std::endl;
+        }
     }
+
+    return 1;
 }
